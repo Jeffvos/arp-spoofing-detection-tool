df85ea4b34b1ed7be6dc2d924295020db43d5318
Jeffrey Vos <jeff.vos@aol.com>
2021-12-01 15:27:06 +0100

    init

---
+++ main.py
@@ -0,0 +1,26 @@
+import os
+from scapy.all import sniff
+
+class PacketScan:
+    def __init__(self):
+        self.ipMacMap = {}
+        print("started scanning for ARP attacks - press ctrl+c to stop")
+        sniff(count=0, filter="arp", store=0, prn=self.scan)
+
+    def scan(self, packet):
+        ip = packet['ARP'].psrc
+        macAdress = packet['Ether'].src
+        if macAdress in self.ipMacMap.keys():
+            if self.ipMacMap[macAdress] != ip:
+                try:
+                    formerIp = self.ipMacMap[macAdress]
+                except:
+                    formerIp = "unknown"
+                msg = f"possible ARP atack detected, device {str(formerIp)} is acting as {str(ip)}"
+
+                return msg
+            else:
+                self.ipMacMap[macAdress] = ip
+     
+if __name__== "__main__":
+    initialization = PacketScan()
